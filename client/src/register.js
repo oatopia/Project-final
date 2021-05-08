035684665fb67771defc2de23d863aa22aed3011
oat <oat15050@gmail.com>
2021-05-08 22:15:14 +0700

    register update

--- client/src/register.js
+++ client/src/register.js
@@ -2,77 +2,69 @@ import './register.css';
 import img from './img/registerimg.png'
 import Navbar from './component/Navbar'
 import Axios from 'axios'
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Auth from './service/authService.js'
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 // import History from './utils/authUtils/History.js'
 
 
 function Register() {
     const url = "https://matching-dorm-tu-server.herokuapp.com/"
     const history = useHistory();
-    const [username,setusername] = useState("");
-    const [password,setpassword] = useState("");
-    const [type,settype] = useState("สมาชิก");
+    const [username, setusername] = useState("");
+    const [password, setpassword] = useState("");
+    const [type, settype] = useState("สมาชิก");
 
-    const addinfo = (e) =>{
+    const addinfo = (e) => {
         e.preventDefault();
-        Axios.post(url+'api/user/register',{
+        Axios.post(url + 'api/user/register', {
             username: username,
             password: password,
             type: type
-        }).then(()=>{
+        }).then(() => {
             console.log("sucess");
         })
     }
 
-
-    const handleRegister=(e)=> {
+    const handleRegister = (e) => {
         e.preventDefault();
-        Auth.register(username,password,type)
-        .then(Response=>{
-            history.push("/login");
-            console.log(Response.data);
+        Auth.register(username, password, type)
+            .then(Response => {
+                history.push("/login");
+                console.log(Response.data);
 
-        })
+            })
     }
-    return(
+    return (
         <>
             <Navbar></Navbar>
-             <div className="container">
-                 <div className="container1">
-                 <form className = "form1" onSubmit={handleRegister}>
-                     <ul className="containerlist">
-                         <h1 id="head-register">สมัครสมาชิก</h1>
-                         <p>ชื่อผู้ใช้</p>
-                         <input className="input-text-register" onChange={(e)=>{
-                             setusername(e.target.value);
-                         }} ></input>
-                         <br></br>
-                         <p>รหัสผ่าน</p>
-                         <input className="input-text-register" onChange={(e)=>{
-                             setpassword(e.target.value);
-                         }}></input>
-                         <br></br>
-                         <p>ประเภทผู้ใช้งาน</p>
-                         <select className="type" onChange={(e)=>{
-                             settype(e.target.value);
-                         }} defaultValue="สมาชิก">
-                             <option value="สมาชิก">สมาชิก</option>
-                             <option value="ผู้ประกอบการ">ผู้ประกอบการ</option>
-                         </select>
-                         <br/><br/>
-                         <button className="buttonregister" type="submit">สมัครสมาชิก</button>
-                         <br/><br/>
-                         
-                     </ul>
-                 </form> 
-                 <img src={img} className="img" width="400" height="500"></img>
-                 </div>
-                        
-             </div>
-            </>
-    );    
+            <div className="container-register">
+                <div className="container1-register">
+                    <form className="form1" onSubmit={handleRegister}>
+                        <h1 id="head-register">สมัครสมาชิก</h1>
+                        <p>ชื่อผู้ใช้</p>
+                        <input className="input-text-register" onChange={(e) => {
+                            setusername(e.target.value);
+                        }} ></input>
+                        <p>รหัสผ่าน</p>
+                        <input className="input-text-register" onChange={(e) => {
+                            setpassword(e.target.value);
+                        }}></input>
+                        <p>ประเภทผู้ใช้งาน</p>
+                        <select className="type" onChange={(e) => {
+                            settype(e.target.value);
+                        }} defaultValue="สมาชิก">
+                            <option value="สมาชิก">สมาชิก</option>
+                            <option value="ผู้ประกอบการ">ผู้ประกอบการ</option>
+                        </select>
+                        <button className="buttonregister-register" type="submit">สมัครสมาชิก</button>
+                    </form>
+                    <img src={img} className="img" width="300" height="500"></img>
+                </div>
+
+            </div>
+        </>
+    );
 }
 
 
