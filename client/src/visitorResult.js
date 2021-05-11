038607a38b1506478c6f17ceb5e454ff53ea17fe
thanapat <oat15050@gmail.com>
2021-05-11 20:59:50 +0700

    edit in visitor model

--- client/src/visitorResult.js
+++ client/src/visitorResult.js
@@ -7,22 +7,105 @@ import imagetest from './img/livingroom1.jpg'
 
 
 function VisitorResult() {
+  const url = "https://matching-dorm-tu-server.herokuapp.com/";
   var location = useLocation();
   const state = location.state;
   console.log("sate match:", state);
+  const [srcimg, setSrcimg] = useState(state.image[0].Image);
+
 
   return (
     <div className="contain-visitor-result">
       <Navbar></Navbar>
-      <div className="content-visitor-result">
-        {state == "" ? <h1>ไม่พบหอพักที่ท่านค้นหา</h1> :
-          <div className="visitor-dorm-block">
-            <label>{state.Dorm_Name}</label>
-            <img src={imagetest} width="20%" height="100%"></img>
-            
+      {state == "" ? <h1>ไม่พบหอพักที่ท่านค้นหา</h1> :
+        <div className="visitor-dorm-block">
+
+          <div className="dorm-data-container">
+            <label className="dormname">{state.information.Dorm_Name}</label>
+            <div className="image-container">
+              <img className="image-big" src={url + "img_Dorm/" + srcimg}></img>
+              <div className="image-mini-container">
+                {state.image.map((data, key) => {
+                  return (
+                    <img className="image-mini" src={url + "img_Dorm/" + data.Image} key={key} ></img>
+                  )
+
+                })}
+              </div>
+            </div>
+
+            <div className="container-block-2">
+              <div className="type-dorm-container">
+                <h3>ประเภทหอพัก</h3>
+                <p>{state.information.Type_D}</p>
+              </div>
+
+              <div className="address-container">
+                <h3>ที่อยู่</h3>
+                <p>{state.information.Address}</p>
+              </div>
+            </div>
+
+            <div className="container-block-3">
+              <div className="fee-container">
+                <h2>รายละเอียดค่าใช้จ่าย</h2>
+                <h3>เงินมัดจำ/ประกัน</h3>
+                <p>{state.information.Address}</p>
+                <h3>อัตราค่าน้ำ</h3>
+                <p>{state.information.Address}</p>
+                <h3>อัตราค่าไฟ</h3>
+                <p>{state.information.Address}</p>
+                <h3>ค่าส่วนกลาง</h3>
+                <p>{state.information.Address}</p>
+              </div>
+
+              <div className="facilities-container">
+                <h2>สิ่งอำนวยความสะดวก</h2>
+                <div className="faciliites-content">
+                  <div className="fac-inner">
+                    <h3>ภายในห้องพัก</h3>
+                    {state.facilities.map(data => {
+                      if (data.Type_F == 'ภายในห้องพัก') {
+                        return (<li>{data.Facility}</li>)
+                      }
+                    })}
+                  </div>
+
+                  <div className="fac-center">
+                    <h3>ส่วนกลาง</h3>
+                    {state.facilities.map(data => {
+                      if (data.Type_F == 'ส่วนกลาง') {
+                        return (<li>{data.Facility}</li>)
+                      }
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+            <div className="container-block-4">
+              <div className="infor-container">
+                <h3>รายละเอียดหอพัก</h3>
+                <p>{state.information.Information}</p>
+              </div>
+
+              <div className="contact-container">
+                <h2>ข้อมูลติดต่อ</h2>
+                <h3>ชื่อผู้ดูแลหอพัก</h3>
+                <p>{state.information.L_name}</p>
+                <h3>เบอร์ติดต่อ</h3>
+                <p>{state.information.Contact_Number}</p>
+                <h3>อีเมล</h3>
+                <p>{state.information.E_mail}</p>
+                <h3>Line ID</h3>
+                <p>{state.information.Line_ID}</p>
+              </div>
+            </div>
+
           </div>
-        }
-      </div>
+        </div>
+      }
     </div>
   );
 }
--- server/model/visitorModel.js
+++ server/model/visitorModel.js
@@ -20,33 +20,40 @@ Visitormodel.searchbyName = (name, result) => {
             console.log("error:", err);
             result(null, err);
             return;
+        } else {
+            if (res.length > 0) {
+                console.log("Response in model", res);
+                dormid = res[0].Dorm_ID;
+                db.query("SELECT * FROM facilities WHERE Dorm_ID = ?", dormid, (err, resfac) => {
+                    if (err) {
+                        console.log("error:", err);
+                        result(null, err);
+                        return;
+                    }
+                    db.query("SELECT * FROM image_dorm WHERE Dorm_ID = ?", dormid, (err, resimg) => {
+                        if (err) {
+                            console.log("error:", err);
+                            result(null, err);
+                            return;
+                        }
+                        dormid = res[0].Dorm_ID;
+                        console.log("image", resimg)
+                        let payload = {
+                            "information": res[0],
+                            "facilities": resfac,
+                            "image": resimg
+                        }
+                        console.log("payload dorm", payload)
+                        result(null, payload);
+                    })
+                })
+            }else{
+                result(null,res);
+            }
         }
 
-        dormid = res[0].Dorm_ID;
-        db.query("SELECT * FROM facilities WHERE Dorm_ID = ?", dormid, (err, resfac) => {
-            if (err) {
-                console.log("error:", err);
-                result(null, err);
-                return;
-            }
-            db.query("SELECT * FROM image_dorm WHERE Dorm_ID = ?", dormid, (err, resimg) => {
-                if (err) {
-                    console.log("error:", err);
-                    result(null, err);
-                    return;
-                }
-                dormid = res[0].Dorm_ID;
-                console.log("image", resimg)
-                let payload={
-                    "information":res[0],
-                    "facilities":resfac,
-                    "image":resimg
-                }
-                console.log("payload dorm", payload)
-                result(null, payload);
-            })
-        })
-        
+
+
     });
 };
 
