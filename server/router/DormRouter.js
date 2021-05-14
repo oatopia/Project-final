cbc140ac628925788d792fd2b7acb3ff35624bb0
thanapat <oat15050@gmail.com>
2021-05-15 02:03:32 +0700

    update v3 dormRouter

--- server/router/DormRouter.js
+++ server/router/DormRouter.js
@@ -2,7 +2,7 @@ import express from 'express'
 import {createDorm, createFacilities, createImage ,getDorm,getDormDataByID,getFacilities,getImage,deletefacbyID,addfac} from '../controller/DormController.js'
 const router = express.Router();
 import {verifyToken} from '../middleware/Verify.js'
-import {upload} from '../middleware/upload.js'
+// import {upload} from '../middleware/upload.js'
 
 router.post('/getDorm',[verifyToken],getDorm);
 router.post('/getDormdatabyId',[verifyToken],getDormDataByID);
@@ -10,7 +10,7 @@ router.post('/getFac',[verifyToken],getFacilities);
 router.post('/getImg',[verifyToken],getImage);
 router.post('/createDorm',[verifyToken],createDorm);
 router.post('/createFacilities',[verifyToken],createFacilities);
-router.post('/createImage',upload.array('Image'),createImage);
+router.post('/createImage',createImage);
 router.delete('/facdeleteDelete/:id',deletefacbyID);
 router.post('/addfacil',[verifyToken],addfac);
 
